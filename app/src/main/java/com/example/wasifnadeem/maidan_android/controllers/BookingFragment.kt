2c83b84b06a7ef949989c1ad40599fd706c6d141
Wasif Nadeem <wasif.nadeem90@gmail.com>
2018-07-05 04:33:18 +0500

    Basic google maps setup

--- app/src/main/java/com/example/wasifnadeem/maidan_android/controllers/BookingFragment.kt
+++ app/src/main/java/com/example/wasifnadeem/maidan_android/controllers/BookingFragment.kt
@@ -1,13 +1,20 @@
 package com.example.wasifnadeem.maidan_android.controllers
 
+import android.location.Location
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.wasifnadeem.maidan_android.R
+import com.google.android.gms.maps.*
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.LatLng
 
-class BookingFragment : Fragment() {
+class BookingFragment : Fragment(), OnMapReadyCallback {
+
+    private var mMap: GoogleMap? = null
+    private lateinit var lastLocation: Location
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -15,5 +22,19 @@ class BookingFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_booking, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val fragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment ;
+        fragment.getMapAsync(this);
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        // Add a marker in Sydney, Australia, and move the camera.
+        val sydney = LatLng(-34.0, 151.0)
+        mMap?.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap?.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
 
 }
