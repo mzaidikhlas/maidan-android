39d191f972e197995e039195083bf09d3a398f5b
Wasif Nadeem <wasif.nadeem90@gmail.com>
2018-07-15 04:39:38 +0500

    Retrofit error fixed

--- app/src/main/java/com/example/wasifnadeem/maidan_android/controllers/HomeFragment.kt
+++ app/src/main/java/com/example/wasifnadeem/maidan_android/controllers/HomeFragment.kt
@@ -2,28 +2,53 @@ package com.example.wasifnadeem.maidan_android.controllers
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.example.wasifnadeem.maidan_android.R
-
-
+import com.example.wasifnadeem.maidan_android.models.User
+import com.example.wasifnadeem.maidan_android.retrofit.ApiInterface
+import com.example.wasifnadeem.maidan_android.retrofit.ApiResponse
+import com.example.wasifnadeem.maidan_android.retrofit.RetrofitClient
+import com.google.android.gms.common.api.Api
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.util.ArrayList
 
 
 class HomeFragment : Fragment() {
 
-//    private lateinit var user:User
+    private lateinit var users: ArrayList<User>
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         val view =  inflater.inflate(R.layout.fragment_home, container, false)
-//            user = User("email", "name", "password", 923214658283, 123456789101234,
-//                    "displayAvatar.jpg", UserRecord("something"))
-
+            apiCall()
         return view
     }
-//    fun apiCall (){
-//        val apiService = ApiClient.getClient()!!.create(ApiInterface::class.java)
-//    }
+    private fun apiCall (){
+
+        val apiService: ApiInterface = RetrofitClient.instance.create(ApiInterface::class.java)
+
+        val call: Call<ApiResponse> = apiService.getAllUsers()
+
+        call.enqueue(object : Callback<ApiResponse>{
+            override fun onFailure(call: Call<ApiResponse>?, t: Throwable?) {
+                Log.d("Error", t!!.message)
+            }
+
+            override fun onResponse(call: Call<ApiResponse>?, response: Response<ApiResponse>?) {
+                Log.d("Response check", "Aya hai")
+                if (response!!.isSuccessful) {
+                    users = ArrayList()
+                    users = response.body()!!.getPayload()
+                }
+            }
+        });
+
+    }
 }
--- app/src/main/java/com/example/wasifnadeem/maidan_android/retrofit/ApiInterface.kt
+++ app/src/main/java/com/example/wasifnadeem/maidan_android/retrofit/ApiInterface.kt
@@ -1,4 +1,4 @@
-package com.example.wasifnadeem.maidan_android
+package com.example.wasifnadeem.maidan_android.retrofit
 
 import retrofit2.Call
 import retrofit2.http.*
